fc966000d9216d27321443f27e50e4be7ed8f38c
jstuckey <jeremystuckey@gmail.com>
2025-08-17 15:24:55 -0400

    Add prompt to app
    This is how we will start the game with the AI agent.

--- README.md
+++ README.md
@@ -9,13 +9,6 @@ The goal is to explore the following technologies:
 - React
 - AI integration
 
-## Initial Prompt
-
-```
-I want to play a text-based adventure game similar to Colossal Cave Adventure, but this game is called Path of the Python.
-Start by saying "You are in a maze of twisty little passages, all alike" and then ask me what I want to do.
-The goal of the game is to find the Golden Python at the end. Mention that somehow in the introduction.
-```
 
 ## Running locally
 
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+FIRST_PROMPT = """
+I want to play a text-based adventure game similar to Colossal Cave Adventure,
+but this game is called Path of the Python. Start by saying "You are in a maze
+of twisty little passages, all alike" and then ask me what I want to do. The goal
+of the game is to find the Golden Python at the end. Mention that in the introduction.
+"""
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "Path of the Python"
