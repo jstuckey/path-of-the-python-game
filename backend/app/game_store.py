a3218fe45668058f4f97a4ddb5ead8556c5e28f6
jstuckey <jeremystuckey@gmail.com>
2025-11-13 17:14:02 -0500

    Add game store object for saving to redis

---
+++ backend/app/game_store.py
@@ -0,0 +1,28 @@
+import os
+
+import redis.asyncio as redis
+
+from game import Game
+
+class GameStore:
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+    redis_client = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_DB,
+        decode_responses=True
+    )
+
+    async def find_by_id(self, id: str) -> Game:
+        serialized_game_state = await self.redis_client.get(id)
+
+        if not serialized_game_state:
+            return None
+        else:
+            return Game.model_validate_json(serialized_game_state)
+
+    async def save(self, id: str, game: Game):
+        await self.redis_client.set(id, game.model_dump_json())
---
+++ backend/tests/test_game_store.py
@@ -0,0 +1,52 @@
+import asyncio
+from unittest.mock import AsyncMock, patch
+
+from game import Game, Message
+from game_store import GameStore 
+
+@patch("game_store.GameStore.redis_client", new_callable=AsyncMock)
+def test_find_by_id(mock_redis_client):
+    game_state = '{"id": "test-game-id", "turn_id": "turn-1", \
+        "messages": [{ "id": "msg-1", "role": "player", "text": "Hello"}]}'
+    mock_redis_client.get.return_value = game_state
+    
+    game = asyncio.run(
+        GameStore().find_by_id("test-game-id")
+    )
+
+    assert game.id == "test-game-id"
+    assert game.turn_id == "turn-1"
+
+    assert game.messages[0].id == "msg-1"
+    assert game.messages[0].role == "player"
+    assert game.messages[0].text == "Hello"
+
+@patch("game_store.GameStore.redis_client", new_callable=AsyncMock)
+def test_find_by_id_not_found(mock_redis_client):
+    mock_redis_client.get.return_value = None
+    
+    game = asyncio.run(
+        GameStore().find_by_id("non-existent-game-id")
+    )
+
+    assert game is None
+
+@patch("game_store.GameStore.redis_client", new_callable=AsyncMock)
+def test_save_game(mock_redis_client):
+    asyncio.run(
+        GameStore().save(
+            "test-game-id",
+            Game(
+                id="test-game-id",
+                turn_id="turn-1",
+                messages=[
+                    Message(id="msg-1", role="player", text="Hello")
+                ]
+            )
+        )
+    )
+
+    mock_redis_client.set.assert_awaited_once_with(
+        "test-game-id",
+        '{"id":"test-game-id","turn_id":"turn-1","messages":[{"id":"msg-1","role":"player","text":"Hello"}]}'
+    )
